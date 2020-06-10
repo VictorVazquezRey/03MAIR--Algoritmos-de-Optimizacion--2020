dc145d9026833caeee0b86324ee8974771b9676a
vazquezrey <vazquezrey@gmail.com>
2020-06-10 08:40:50 +0200

    Final de hanoi.py y creación de reinas.py

--- Ejercicios/hanoi.py
+++ Ejercicios/hanoi.py
@@ -1,2 +1,17 @@
-def hanoi(n:int):
-    pass
+def hanoi(n:int,origin:list, helper:list, target:list, game: list):
+    if n > 0:
+        hanoi(n-1, origin, target, helper, game)
+        target.append(origin.pop())
+        print(game)
+        hanoi(n-1, helper, origin, target, game)
+
+# These are like stacks. More to the rigth more up
+origin = [5, 4, 3, 2, 1]
+helper = []
+target = []
+
+# This is used to print, because origin, helper and target are changed
+# across calls. 
+game = [origin, helper, target]
+
+hanoi(5, origin, helper, target, game)
