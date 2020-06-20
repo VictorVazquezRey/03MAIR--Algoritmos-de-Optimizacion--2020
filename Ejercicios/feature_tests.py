436b28e0fec5646383b2138bec03d9ea64db1a81
vazquezrey <vazquezrey@gmail.com>
2020-06-20 22:10:01 +0200

    creation of tasks_assignament_brute_force.py

--- Ejercicios/feature_tests.py
+++ Ejercicios/feature_tests.py
@@ -7,10 +7,11 @@ ax = plt.axes(projection="3d")
 
 
 def f(x:float, y: float) -> float:
-    return np.sin((x**2)/2-1/4*y**2+3) * np.cos(2*x+1-np.exp(y))
+    return np.sin(1/2 * x**2 - 1/4 * y**2 + 3) * np.cos(2*x + 1 - np.e**y)
 
 
-x = np.linspace(-2*np.pi,2*np.pi,100)
+# x = np.linspace(-np.pi,np.pi,100)
+x = np.linspace(-2, 2, 100)
 y = x
 X, Y = np.meshgrid(x,y)
 Z = f(X,Y)
---
+++ Ejercicios/tasks_assignament_brute_force.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+agents = 4
+jobs = 4
+benefit_matrix = np.random.randint(1, 26, size=(agents, jobs))
+print(benefit_matrix)
