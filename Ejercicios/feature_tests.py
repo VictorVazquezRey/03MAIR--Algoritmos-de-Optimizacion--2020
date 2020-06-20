78b3426a6e20b2cd7b0a7f2f75d9e704484bb247
vazquezrey <vazquezrey@gmail.com>
2020-06-20 18:08:19 +0200

    test 3D in mathplotlib

--- Ejercicios/feature_tests.py
+++ Ejercicios/feature_tests.py
@@ -4,3 +4,17 @@ import matplotlib.pyplot as plt
 
 fig = plt.figure()
 ax = plt.axes(projection="3d")
+
+
+def f(x:float, y: float) -> float:
+    return np.sin((x**2)/2-1/4*y**2+3) * np.cos(2*x+1-np.exp(y))
+
+
+x = np.linspace(-2*np.pi,2*np.pi,100)
+y = x
+X, Y = np.meshgrid(x,y)
+Z = f(X,Y)
+
+#ax.plot_wireframe(X,Y,Z)
+ax.plot_surface(X,Y,Z,cmap="viridis")
+plt.show()
