cd68453908db2b7cdb97d82ba50a94b4bd206c27
vazquezrey <vazquezrey@gmail.com>
2020-06-21 12:56:49 +0200

    lower_bound_cost is now lower_and_upper_bound_cost and calculate both bounds

--- Ejercicios/tasks_assignament_branch_and_bound.py
+++ Ejercicios/tasks_assignament_branch_and_bound.py
@@ -8,23 +8,27 @@ def cost(costs_matrix: 'nparray', assignament: tuple) -> float:
         cost += costs_matrix[agent][job]
     return cost
 
-def lower_bound_cost(costs_matrix: 'nparray', assignament: tuple) -> float:
+
+def lower_and_upper_bound_cost(costs_matrix: 'nparray', assignament: tuple) -> tuple:
     dimension = costs_matrix.shape[0]
     fixed_cost = cost(costs_matrix, assignament)
 
     unassigned_tasks = set(range(dimension)) - set(assignament)
     unassigned_agents = range(len(assignament), dimension)
 
-    estimated_cost = 0
+    estimated_min_cost = 0
+    estimated_max_cost = 0
     for agent in unassigned_agents:
-        estimated_cost += min([costs_matrix[agent][task]  for task in unassigned_tasks])
+        costs_for_agent_rest_of_tasks = [costs_matrix[agent][task] for task in unassigned_tasks]
+        estimated_min_cost += min(costs_for_agent_rest_of_tasks)
+        estimated_max_cost += max(costs_for_agent_rest_of_tasks)
 
-    return fixed_cost + estimated_cost
+    return fixed_cost + estimated_max_cost, fixed_cost + estimated_min_cost
 
 
-def branch(parent: tuple, dimension:int)->list:
+def branch(parent: dict, dimension: int) -> list:
     to_expand = set(range(dimension)) - set(parent)
-    return [parent+(task,) for task in to_expand]
+    return [parent + (task,) for task in to_expand]
 
 
 def assignaments(cost_matrix: 'nparray') -> tuple:
--- Ejercicios/test_branch_and_bound.py
+++ Ejercicios/test_branch_and_bound.py
@@ -11,12 +11,15 @@ class MyTestCase(unittest.TestCase):
     #  [13 14 15 16]]
 
     def test_brach(self):
-        self.assertEqual([(0,1), (0,2), (0,3)], to_test.branch((0,),self.dimension))
+        # {'assig':(0,), 'lb':0, 'ub':0}
+        self.assertEqual([(0,1), (0,2), (0,3)], to_test.branch((0,), self.dimension))
         self.assertEqual([(0, 2, 1), (0, 2, 3)], to_test.branch((0,2), self.dimension))
 
 
     def test_lower_bound_cost(self):
-        self.assertEqual(33, to_test.lower_bound_cost(self.costs_matrix,(0,1)))
+        max, min = to_test.lower_and_upper_bound_cost(self.costs_matrix,(0,1))
+        self.assertEqual(33, min)
+        self.assertEqual(35, max)
 
 if __name__ == '__main__':
     unittest.main()
