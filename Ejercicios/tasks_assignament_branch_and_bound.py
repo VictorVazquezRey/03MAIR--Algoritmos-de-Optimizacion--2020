9bce564eed7289d7b68bf9866d5302504a7e4270
vazquezrey <vazquezrey@gmail.com>
2020-06-21 15:35:40 +0200

    Refactor into a class Task_Asignament

--- Ejercicios/tasks_assignament_branch_and_bound.py
+++ Ejercicios/tasks_assignament_branch_and_bound.py
@@ -2,43 +2,58 @@ import numpy as np
 from math import inf
 
 
-def cost(costs_matrix: 'nparray', assignament: tuple) -> float:
-    cost = 0
-    for job, agent in enumerate(assignament):
-        cost += costs_matrix[agent][job]
-    return cost
+class Task_Asignament:
+    dimension = None
+    costs_matrix = None
+
+    def __init__(self, dimension, costs_matrix=None, less_cost=1, high_cost=25):
+        self.dimension = dimension
+        if costs_matrix is None:
+            self.costs_matrix = np.random.randint(less_cost,
+                                                  high_cost + 1,
+                                                  size=(dimension, dimension))
+        else:
+            self.costs_matrix = costs_matrix
 
 
-def lower_and_upper_bound_cost(costs_matrix: 'nparray', assignament: tuple) -> tuple:
-    dimension = costs_matrix.shape[0]
-    fixed_cost = cost(costs_matrix, assignament)
+    def cost(self, assignament: tuple) -> float:
+        cost = 0
+        for agent, job in enumerate(assignament):
+            cost += self.costs_matrix[agent][job]
+        return cost
 
-    unassigned_tasks = set(range(dimension)) - set(assignament)
-    unassigned_agents = range(len(assignament), dimension)
+    def lower_and_upper_bound_cost(self, assignament: tuple) -> tuple:
+        fixed_cost = self.cost(assignament)
 
-    estimated_min_cost = 0
-    estimated_max_cost = 0
-    for agent in unassigned_agents:
-        costs_for_agent_rest_of_tasks = [costs_matrix[agent][task] for task in unassigned_tasks]
-        estimated_min_cost += min(costs_for_agent_rest_of_tasks)
-        estimated_max_cost += max(costs_for_agent_rest_of_tasks)
+        unassigned_tasks = set(range(self.dimension)) - set(assignament)
+        unassigned_agents = range(len(assignament), self.dimension)
 
-    return fixed_cost + estimated_max_cost, fixed_cost + estimated_min_cost
+        estimated_min_cost = 0
+        estimated_max_cost = 0
+        for agent in unassigned_agents:
+            costs_for_agent_rest_of_tasks = [self.costs_matrix[agent][task] for task in unassigned_tasks]
+            estimated_min_cost += min(costs_for_agent_rest_of_tasks)
+            estimated_max_cost += max(costs_for_agent_rest_of_tasks)
+
+        return fixed_cost + estimated_max_cost, fixed_cost + estimated_min_cost
+
+    def branch(self, parent: dict) -> list:
+        to_expand = set(range(self.dimension)) - set(parent['node'])
+        childs = []
+        for task in to_expand:
+            node = parent['node'] + (task,)
+            max, min = self.lower_and_upper_bound_cost(node)
+            childs.append({'node': node, 'lb': min, 'ub': max})
+
+        return childs
+
+    def assignaments(self) -> tuple:
+        min_cost = inf
+        result = ()
+
+        return result, min_cost
 
 
-def branch(parent: dict, dimension: int) -> list:
-    to_expand = set(range(dimension)) - set(parent)
-    return [parent + (task,) for task in to_expand]
-
-
-def assignaments(cost_matrix: 'nparray') -> tuple:
-    dimension = costs_matrix.shape[0]
-    min_cost = inf
-    result = ()
-
-    return result, min_cost
-
-
-dimension = 4
+# dimension = 4
 # generación automática y aleatoria de la matriz de costes.
-costs_matrix = np.random.randint(1, 26, size=(dimension, dimension))
+# costs_matrix = np.random.randint(1, 26, size=(dimension, dimension))
--- Ejercicios/test_branch_and_bound.py
+++ Ejercicios/test_branch_and_bound.py
@@ -2,24 +2,35 @@ import unittest
 import tasks_assignament_branch_and_bound as to_test
 import numpy as np
 
+
 class MyTestCase(unittest.TestCase):
-    dimension = 4
-    costs_matrix = np.array([i for i in range(1, dimension*dimension + 1)]).reshape((dimension,dimension))
-    # [[1  2  3  4]
-    #  [5  6  7  8]
-    #  [9 10 11 12]
-    #  [13 14 15 16]]
+
+    def setUp(self) -> None:
+        self.dimension = 4
+        self.costs_matrix = np.array([i for i in range(1, self.dimension * self.dimension + 1)])\
+                                    .reshape((self.dimension, self.dimension))
+        self.ta = to_test.Task_Asignament(self.dimension,costs_matrix=self.costs_matrix)
+        # [[1  2  3  4]
+        #  [5  6  7  8]
+        #  [9 10 11 12]
+        #  [13 14 15 16]]
 
     def test_brach(self):
-        # {'assig':(0,), 'lb':0, 'ub':0}
-        self.assertEqual([(0,1), (0,2), (0,3)], to_test.branch((0,), self.dimension))
-        self.assertEqual([(0, 2, 1), (0, 2, 3)], to_test.branch((0,2), self.dimension))
+        {'assig':(0,), 'lb':0, 'ub':0}
+        self.assertListEqual([{'node': (0, 1), 'lb': 33, 'ub': 35},
+                              {'node': (0, 2), 'lb': 32, 'ub': 36},
+                              {'node': (0, 3), 'lb': 33, 'ub': 35}],
+                             self.ta.branch({'node': (0,), 'lb': 0, 'ub': 0}))
 
+        self.assertEqual([{'lb': 34, 'node': (0, 2, 1), 'ub': 34},
+                          {'lb': 34, 'node': (0, 2, 3), 'ub': 34}],
+                         self.ta.branch({'node': (0, 2), 'lb': 0, 'ub': 0}))
 
     def test_lower_bound_cost(self):
-        max, min = to_test.lower_and_upper_bound_cost(self.costs_matrix,(0,1))
+        max, min = self.ta.lower_and_upper_bound_cost((0, 1))
         self.assertEqual(33, min)
         self.assertEqual(35, max)
+
 
 if __name__ == '__main__':
     unittest.main()
